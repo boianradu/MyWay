81dc61a2352a4a2cb09b347e928a0c2a4e149728
Radu Boian <boianradu@gmail.com>
2021-05-10 21:40:37 +0300

    feat: bottom navigation and snackbar

---
+++ lib/widgets/alert_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SnackBarMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Ok',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
+      ),
+    );
+  }
+}
